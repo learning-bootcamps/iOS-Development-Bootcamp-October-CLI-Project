661509c495199327689f29e36c2c67075312e74f
Fahad Matlagitu <matlagitu@gmail.com>
2023-10-21 15:06:12 +0300

    added Quiz questions

--- Quiz CLI App/main.swift
+++ Quiz CLI App/main.swift
@@ -7,6 +7,13 @@
 
 import Foundation
 
+let questionAsString = """
+What is Git?:A version control system.:A nickname for GitHub.:A remote repository platform.:A programming language.:0
+What is the command to get the installed version of Git?:git --version.:gitVersion.:git help version.:getGitVersion.:0
+Which option should you use to set the default user name for every repository on your computer?:--A.:--global.:--all.:No need to specify, that is the default.:1
+What is the command to set the user email for the current repository?:git email.user.:git config user.email.:git config email.:git switch user.email.:1
+What is the command to initialize Git on the current repository?:initialize git.:git start.:start git.:git init.:3
+"""
 
 
 func LoadQuestions() {
