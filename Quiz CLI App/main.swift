c7cae8e1e3e23f4aa8cee5042a2867cedebb3bd8
Fahad Matlagitu <matlagitu@gmail.com>
2023-10-21 15:03:43 +0300

    while true loop

--- Quiz CLI App/main.swift
+++ Quiz CLI App/main.swift
@@ -7,5 +7,32 @@
 
 import Foundation
 
-print("Hello, World!")
 
+
+func LoadQuestions() {
+    print("Questions loaded successfully.") //--> adding by me
+}
+
+func startQuiz() {
+    print("If you want to load the quiz, please enter 1. If not, please enter 0")
+
+    while true  {
+        if let readInput = readLine() {
+            do {
+                if let choice =  Int(readInput) {
+                    if choice == 1 {
+                        LoadQuestions()
+                    } else if choice == 0 {
+                        break
+                    } else {
+                        print("Please pick between 0 and 1")
+                    }
+                } else {
+                    print("Please enter a valid number (0 or 1)")
+                }
+            }
+        }
+    }
+}
+
+startQuiz()
